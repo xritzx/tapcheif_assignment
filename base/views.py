781ff07e46cb03a662d17bbcedfe237602d1f200
xritzx <ritankarpaul47@gmail.com>
2019-12-02 10:48:34 +0530

    Minor bug fix

--- base/views.py
+++ base/views.py
@@ -24,22 +24,29 @@ def index(request):
         if text_form.is_valid():
             passage = ""
             if 'pdf_file' in request.FILES:
+                # If pdf is uploaded then pdf is priortized
                 pdf = request.FILES['pdf_file']
                 fileSys = FileSystemStorage()
                 filename = fileSys.save(pdf.name, pdf)
                 url = fileSys.url(filename)
+                # Pdf doc is stored in server for processing.
                 path = os.path.join(settings.MEDIA_ROOT, filename)
                 pdfFileObj = open(path, 'rb') 
+                # Heroku doesn't allow Media transactions on free tier
                 pdfReader = PyPDF2.PdfFileReader(pdfFileObj) 
                 for i in range(pdfReader.numPages):
                     passage += pdfReader.getPage(i).extractText()
                 pdfFileObj.close()
             else:
                 passage = text_form.cleaned_data["text"]
-            inv_index.reset()
-            documents = passage.splitlines()
-            for i in range(len(documents)):
-                inv_index.invertify_document({"id": i, "text": documents[i]})
+
+            inv_index.reset() #reseting index table 
+            
+            iterator = 0 # Document iterator
+            for i in passage.splitlines():
+                if(len(i)>0):
+                    inv_index.invertify_document({"id": iterator, "text": i})
+                    iterator+=1                
 
         response = render(request, 'index.html', {'text_form': text_form, 'passage':passage})
         response.set_cookie('passage', passage)
